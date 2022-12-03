a1ebcabf16141c8bed79d01b01e01c702a02252e
Minse5k <81351313+Minse5k@users.noreply.github.com>
2022-12-03 21:16:34 +0900

    Create PM_징검다리건너기.js

---
+++ Programmers/PM_징검다리건너기.js
@@ -0,0 +1,20 @@
+function solution(stones, k) {
+    let left = 1;
+    let right = 200000000;
+    
+    while(left <= right) {
+        const mid = Math.floor((left + right) /2)
+        let cnt = 0;
+        for(const num of stones) {
+            if(num-mid <= 0) cnt++;
+            else cnt = 0;
+            if(cnt >= k) break;
+        }
+        if(cnt >= k) right = mid-1;
+        else {
+            left = mid + 1;
+        }
+    }
+    
+    return left;
+}
