3e0d70194124fb08bba504e24a07da5588e54b50
Minse5k <81351313+Minse5k@users.noreply.github.com>
2022-12-08 16:57:17 +0900

    Create BJ_G5_14567_선수과목.js

---
+++ G5/BJ_G5_14567_선수과목.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = fs.readFileSync(filePath).toString().split("\r\n");
+const [N, M] = input[0].split(" ").map((v) => parseInt(v));
+const arr = [];
+for (let i = 1; i <= M; i++) {
+  arr.push(input[i].split(" ").map((v) => parseInt(v)));
+}
+arr.sort((a, b) => {
+  if (a[0] === b[0]) {
+    a[1] - b[1];
+  }
+  return a[0] - b[0];
+});
+const dp = Array.from({ length: N }, () => 1);
+arr.forEach(([A, B]) => {
+  dp[B - 1] = Math.max(dp[B - 1], dp[A - 1] + 1);
+});
+console.log(dp.join(" "));
