296bf71aa136d40e09b097f727255f9bdaece0bf
Minse5k <81351313+Minse5k@users.noreply.github.com>
2023-02-28 18:15:49 +0900

    Create PM_프린터.js

---
+++ Programmers/PM_프린터.js
@@ -0,0 +1,23 @@
+function getCount(priorities, location) {
+    const n = priorities.length;
+    let i = 0;
+    let count = 0;
+    
+    while(1) {
+        let now = i % n;
+        const max = Math.max(...priorities);
+        
+        if(priorities[now] === max) {
+            count++;
+            if(now === location) {
+                return count;
+            }
+            priorities[now] = -1;
+        }
+        i++;
+    }
+}
+
+function solution(priorities, location) {
+    return getCount(priorities, location)
+}
