7b8060e4e5f636e5ed8820ba2711be10e7302d4c
Minse5k <81351313+Minse5k@users.noreply.github.com>
2023-04-22 23:00:39 +0900

    Update BJ_G5_11000_강의실배정.java

--- G5/BJ_G5_11000_강의실배정.java
+++ G5/BJ_G5_11000_강의실배정.java
@@ -16,7 +16,7 @@ class Lecture {
         this.end = end;
     }
 }
-
+       
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
